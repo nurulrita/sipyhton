f85aa9e217de51c0e4398128d5252ac1a27d5baa
nuthanu <nuthanuu@gmail.com>
2017-03-21 15:12:16 +0700

    poll app

--- poll/urls.py
+++ poll/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^$', views.index, name= 'index'),
     url(r'^help_me/$', views.help_me, name= 'help_me'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail_question, name='detail_question'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^vote/$', views.vote, name= 'vote'),
     
 ]
 
--- poll/views.py
+++ poll/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404, render
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -18,3 +21,15 @@ def detail_question(request, question_id):
 	question = Question.objects.get(id=question_id)
 	context ['question'] = question
 	return render (request, 'poll/detail_question.html', context)
+
+def vote(request):
+	context = {}
+	choice_id = request.POST['choice']
+	selected_choice = Choice.objects.get(id=int(choice_id))
+	selected_choice.votes += 1
+	selected_choice.save()
+	return HttpResponseRedirect(reverse('poll:results', args=(selected_choice.question.id,)))
+
+def results(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    return render(request, 'poll/results.html', {'question': question})
