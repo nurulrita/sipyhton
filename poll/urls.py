31cc56a67f3c350bf0af1d98b75627e9fb87e1e5
nuthanu <nuthanuu@gmail.com>
2017-03-21 12:22:28 +0700

    choice image

--- poll/urls.py
+++ poll/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 
 
+
 from . import views
 
 urlpatterns = [
 	# /poll/
     url(r'^$', views.index, name= 'index'),
     url(r'^help_me/$', views.help_me, name= 'help_me'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail_question, name='detail_question'),
     
 ]
+
--- poll/views.py
+++ poll/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
+from .models import Question
+
 # Create your views here.
 def index(request):
 	context = {}
+	questions_query = Question.objects.all()
+	context ['questions'] = questions_query
 	return render(request, 'poll/index.html', context)
 
 def help_me(request):
 	context = {}
 	return render(request, 'poll/help_me.html', context)
+
+def detail_question(request, question_id):
+	context ={}
+	question = Question.objects.get(id=question_id)
+	context ['question'] = question
+	return render (request, 'poll/detail_question.html', context)
